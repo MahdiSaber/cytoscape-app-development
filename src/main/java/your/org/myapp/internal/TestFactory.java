a52ad4847d895143aa566c40b8abbfd8a29bf833
MahdiSaber <mahdi.saber.de+github@gmail.com>
2016-06-09 11:25:34 +0200

    Initial Introduction to CytoScape App/Plugin Development

---
+++ src/main/java/your/org/myapp/internal/TestFactory.java
@@ -0,0 +1,15 @@
+package your.org.myapp.internal;
+
+import org.cytoscape.work.AbstractTaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+public class TestFactory extends AbstractTaskFactory
+{
+
+	@Override
+	public TaskIterator createTaskIterator()
+	{
+		return null;
+	}
+
+}
